7da81980b9f9d2c1cacf471ba9cfe14540160006
guilherme-w8 <gtavares739@gmail.com>
2024-10-27 00:23:42 -0300

    add: Console Calculator using C Sharp

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,19 @@
 ﻿Console.Clear();
 Console.WriteLine("Calculadora em C Sharp");
+
+var calculator = new Calculator.Calculator();
+
+Console.WriteLine("Write two values");
+
+Console.Write("> ");
+double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("> ");
+double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.Clear();
+
+calculator.Sum(firstNumber, secondNumber);
+calculator.Sub(firstNumber, secondNumber);
+calculator.Divisor(firstNumber, secondNumber);
+calculator.Times(firstNumber, secondNumber);

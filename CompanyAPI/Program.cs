712dc8b5d121cc60e52d80bf59e59661e5c95e7b
guilherme-w8 <gtavares739@gmail.com>
2024-11-24 20:22:27 -0300

    Scoped Interface and Service

--- CompanyAPI/Program.cs
+++ CompanyAPI/Program.cs
@@ -1,4 +1,5 @@
 using CompanyAPI.Data;
+using CompanyAPI.Services.Entrepreneur;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IEntrepreneurInterface, EntrepreneurService>();
 
 var app = builder.Build();
 

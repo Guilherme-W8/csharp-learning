f53c637a4c999398dde73200bdaa2e82327db283
guilherme-w8 <gtavares739@gmail.com>
2024-11-24 20:22:02 -0300

    Default Struct Folders

---
+++ CompanyAPI/Services/Entrepreneur/EntrepreneurService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyAPI.Services.Entrepreneur
+{
+    public class EntrepreneurService : IEntrepreneurInterface
+    {
+
+    }
+}

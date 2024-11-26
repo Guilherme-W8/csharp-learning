bd2ba3b787cc0f785cd4497cf9c7840737dca9fd
guilherme-w8 <gtavares739@gmail.com>
2024-11-25 22:03:59 -0300

    service functions

--- CompanyAPI/Services/Entrepreneur/EntrepreneurService.cs
+++ CompanyAPI/Services/Entrepreneur/EntrepreneurService.cs
@@ -2,11 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CompanyAPI.Data;
+using CompanyAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CompanyAPI.Services.Entrepreneur
 {
     public class EntrepreneurService : IEntrepreneurInterface
     {
+        private readonly AppDbContext _context;
+        public EntrepreneurService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task<ResponseModel<EntrepreneurModel>> FindEntrepreneurByCompanyId(int companyID)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<ResponseModel<EntrepreneurModel>> FindEntrepreneurById(int entrepreneurID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ResponseModel<List<EntrepreneurModel>>> ListEntrepreneurs()
+        {
+            ResponseModel<List<EntrepreneurModel>> response = new ResponseModel<List<EntrepreneurModel>>();
+
+            try
+            {
+                var entrepreneurs = await _context.Entrepreneurs.ToListAsync();
+                response.Data = entrepreneurs;
+                response.Message = "All entrepreneurs returned successfully";
+
+                return response;
+            }
+            catch (Exception e)
+            {
+                response.Message = e.Message;
+                response.Status = false;
+                return response;
+            }
+        }
     }
 }
--- CompanyAPI/Services/Entrepreneur/IEntrepreneurInterface.cs
+++ CompanyAPI/Services/Entrepreneur/IEntrepreneurInterface.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CompanyAPI.Models;
 
 namespace CompanyAPI.Services.Entrepreneur
 {
     public interface IEntrepreneurInterface
     {
-        
+        Task<ResponseModel<List<EntrepreneurModel>>> ListEntrepreneurs();
+        Task<ResponseModel<EntrepreneurModel>> FindEntrepreneurById(int entrepreneurID);
+        Task<ResponseModel<EntrepreneurModel>> FindEntrepreneurByCompanyId(int companyID);
     }
 }

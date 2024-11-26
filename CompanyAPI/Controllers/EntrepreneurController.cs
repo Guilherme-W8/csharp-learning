702fe86bea904c539525a53cd8f7def16c0fc55d
guilherme-w8 <gtavares739@gmail.com>
2024-11-25 22:04:40 -0300

    GET Entrepreneurs

--- CompanyAPI/Controllers/EntrepreneurController.cs
+++ CompanyAPI/Controllers/EntrepreneurController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CompanyAPI.Models;
+using CompanyAPI.Services.Entrepreneur;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyAPI.Controllers
@@ -10,6 +12,17 @@ namespace CompanyAPI.Controllers
     [Route("api/[controller]")]
     public class EntrepreneurController : ControllerBase
     {
-        
+        private readonly IEntrepreneurInterface _entrepreneurInterface;
+        public EntrepreneurController(IEntrepreneurInterface entrepreneurInterface)
+        {
+            _entrepreneurInterface = entrepreneurInterface;
+        }
+
+        [HttpGet("ListEntrepreneurs")]
+        public async Task<ActionResult<ResponseModel<List<EntrepreneurModel>>>> ListEntrepreneurs()
+        {
+            var entrepreneurs = await _entrepreneurInterface.ListEntrepreneurs();
+            return Ok(entrepreneurs);
+        }
     }
 }

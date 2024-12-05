4a2d84c753c80431d0db7dc94496f1d0503c5594
guilherme-w8 <gtavares739@gmail.com>
2024-12-04 23:24:01 -0300

    DTO de vinculo

---
+++ CompanyAPI/DTO/Bond/EntrepreneurBondDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyAPI.DTO.Bond
+{
+    public class EntrepreneurBondDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
